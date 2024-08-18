e8ed509c195a9d7d15fc633ada00da57122fedc2
IanMartinezCastro <imartinez90884@ufide.ac.cr>
2024-08-18 12:54:41 -0600

    carga con cambios

--- TravelFidHub/src/main/java/com/mycompany/travelfidhub/TravelFidHub.java
+++ TravelFidHub/src/main/java/com/mycompany/travelfidhub/TravelFidHub.java
@@ -11,6 +11,6 @@ package com.mycompany.travelfidhub;
 public class TravelFidHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Yo");
     }
 }
