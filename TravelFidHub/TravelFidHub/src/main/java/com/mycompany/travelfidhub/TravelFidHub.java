5f2578d20b288368075b266a5da96de6254582cd
IanMartinezCastro <imartinez90884@ufide.ac.cr>
2024-08-18 12:51:59 -0600

    Add files via upload

---
+++ TravelFidHub/TravelFidHub/src/main/java/com/mycompany/travelfidhub/TravelFidHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.travelfidhub;
+
+/**
+ *
+ * @author user
+ */
+public class TravelFidHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
