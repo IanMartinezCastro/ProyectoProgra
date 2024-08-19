416e313696f387038230d26453ccd99072771b9c
dancastros <40213363+dancastros@users.noreply.github.com>
2024-08-18 22:32:49 -0600

    Add files via upload

--- TravelFidHub/src/main/java/Modelo/Usuario.java
+++ TravelFidHub/src/main/java/Modelo/Usuario.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author user
- */
 public class Usuario {
+    
+    private String nombre, apellido, correo, prreferenciasViaje, preferenciasContactos;    
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String correo, String prreferenciasViaje, String preferenciasContactos) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.prreferenciasViaje = prreferenciasViaje;
+        this.preferenciasContactos = preferenciasContactos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPrreferenciasViaje() {
+        return prreferenciasViaje;
+    }
+
+    public void setPrreferenciasViaje(String prreferenciasViaje) {
+        this.prreferenciasViaje = prreferenciasViaje;
+    }
+
+    public String getPreferenciasContactos() {
+        return preferenciasContactos;
+    }
+
+    public void setPreferenciasContactos(String preferenciasContactos) {
+        this.preferenciasContactos = preferenciasContactos;
+    }
+    
+    
     
 }
